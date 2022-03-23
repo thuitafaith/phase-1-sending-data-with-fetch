69b42cfcb296862064dd4a20058c5c3cef41c1ab
thuitaf <faith.thuita@moringaschool.com>
2022-03-23 16:59:01 +0300

    commit tree 8def274d7620f6e74e96893aabed3bfa8caca9f4

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your code here
+// const Data = {
+//     name:'',
+//     email:"",
+// }
+
+
+function submitData(name,email){
+    const config = {
+        method:"POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
+          body: JSON.stringify({name,email}),
+        
+    }
+    return fetch('http://localhost:3000/users', config)
+            .then( function ( response ) {
+                return response.json()
+        } )
+        .then( function ( object ) {
+            document.body.innerHTML = object[ "id" ]
+        } )
+        .catch( function ( error ) {
+            document.body.innerHTML = error.message
+        } ) 
+
+
+
+
+
+}
